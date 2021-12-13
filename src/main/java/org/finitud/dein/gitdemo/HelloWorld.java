d9bcee60b6723b6c80cf519cac80d58ed82e9d5d
Alicia Martín <alicew.garmar@educa.jcyl.es>
2021-12-13 19:36:39 +0100

    Añadir clase principal

---
+++ src/main/java/org/finitud/dein/gitdemo/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.finitud.dein.gitdemo;
+
+/**
+ *
+ * @author Alicia Martin
+ */
+public class HelloWorld extends javax.swing.JFrame {
+    public static void main(String[] args) {
+        HelloWorld app = new HelloWorld();
+        app.setSize(300, 200);
+        app.setTitle("Hola Mundo");
+        app.setVisible(true);
+    }
+}
